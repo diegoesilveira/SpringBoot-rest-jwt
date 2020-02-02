432e705d37fcadfad8d25091f9a036098d451f61
diego <diego@DESKTOP-6LKLR8F>
2020-02-01 22:07:40 -0300

    Criado o pacote repository e a interface CategoriaRepository.

---
+++ springboot/src/main/java/br/com/diego/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package br.com.diego.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.diego.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
