5e6b3e1facec428b3258cf40ba0784979e8ad973
diego <diego@DESKTOP-6LKLR8F>
2020-02-02 04:41:44 -0300

    Criado ProdutoRepository, realizado relacionamentos da classe Produto e Categoria.

--- springboot/src/main/java/br/com/diego/SpringbootApplication.java
+++ springboot/src/main/java/br/com/diego/SpringbootApplication.java
@@ -1,13 +1,30 @@
 package br.com.diego;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootApplication {
+import br.com.diego.repositories.CategoriaRepository;
+import br.com.diego.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class SpringbootApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
 	}
 
 }
---
+++ springboot/src/main/java/br/com/diego/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.diego.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.diego.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+	
+
+}
