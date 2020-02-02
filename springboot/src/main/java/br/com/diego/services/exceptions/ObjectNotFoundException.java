8bdedbeab8a8a931211bf3cba43616a742364602
diego <diego@DESKTOP-6LKLR8F>
2020-02-02 09:30:37 -0300

    Criado a exceção ObjectNotFoundException.

---
+++ springboot/src/main/java/br/com/diego/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package br.com.diego.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public ObjectNotFoundException(String mensagem, Throwable cause) {
+		super(mensagem, cause);
+	}
+}
