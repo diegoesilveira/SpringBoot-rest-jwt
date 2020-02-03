50ff3bafc81cab9944afbdd7b13c2c13561c88f8
diego <diego@DESKTOP-6LKLR8F>
2020-02-02 23:51:38 -0300

    Realizado relacionamento das classes Cliente, telefone e Endereco

--- springboot/src/main/java/br/com/diego/domain/Endereco.java
+++ springboot/src/main/java/br/com/diego/domain/Endereco.java
@@ -1,7 +1,21 @@
 package br.com.diego.domain;
 
-public class Endereco {
-	
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Integer id;
 	private String logradouro;
 	private String numero;
@@ -9,14 +23,20 @@ public class Endereco {
 	private String bairro;
 	private String cep;
 	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
+	@ManyToOne
+	@JoinColumn(name = "cidade_id")
+	private Cidade cidade;
 	
 	public Endereco() {
 		
 	}
 
 	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
-			Cliente cliente) {
+			Cliente cliente, Cidade cidade) {
 		
 		this.id = id;
 		this.logradouro = logradouro;
@@ -25,6 +45,7 @@ public class Endereco {
 		this.bairro = bairro;
 		this.cep = cep;
 		this.cliente = cliente;
+		this.cidade = cidade;
 	}
 
 	public Integer getId() {
@@ -81,6 +102,14 @@ public class Endereco {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
 	}
 
 	@Override
