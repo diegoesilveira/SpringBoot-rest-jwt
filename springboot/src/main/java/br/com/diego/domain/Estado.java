9468b181d71c549e12a71ad815f1fef63f9cc5cb
diego <diego@host.docker.internal>
2020-03-05 05:04:28 -0300

    Atualizando projeto.

--- springboot/src/main/java/br/com/diego/domain/Estado.java
+++ springboot/src/main/java/br/com/diego/domain/Estado.java
@@ -3,14 +3,14 @@ package br.com.diego.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Id;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Estado implements Serializable{
