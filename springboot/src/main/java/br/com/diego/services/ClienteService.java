6a1306af1f6add6c09730e37c5bb422bceb19f03
Diego Silveira <diego.silveira@gx2.com.br>
2020-03-05 09:19:25 -0300

    Teste

--- springboot/src/main/java/br/com/diego/services/ClienteService.java
+++ springboot/src/main/java/br/com/diego/services/ClienteService.java
@@ -18,7 +18,7 @@ public class ClienteService {
 	public Cliente buscar(Integer id) {
 		Optional <Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		
 	}
 }
